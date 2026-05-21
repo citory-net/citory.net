a118ae5679c242a21eded89d117395a2028ba5ed
Nionim <delta.cion.fe@gmail.com>
2026-05-21 04:18:11 +0300

    some fixes

--- .root/run.py
+++ .root/run.py
@@ -1,10 +1,18 @@
-#   ./.root/run.py
 import http.server
 import socketserver
+import subprocess
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(ROOT_DIR)
 
 PORT = 1663
 Handler = http.server.SimpleHTTPRequestHandler
 
+os.chdir(BASE_DIR)
+
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    subprocess.run([sys.executable, os.path.join(ROOT_DIR, "generate_assets.py")])
     print(f"Server started on: http://localhost:{PORT}")
     httpd.serve_forever()
